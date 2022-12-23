a5ba1fac9daa7b957ac10de5548c6d7b2c193d27
Jordan Speicher <jordan@jspeicher.com>
2022-12-23 14:39:55 -0600

    Extensive rework to state machine
    * Move all state transitions to one method
    * Added error handling via Error event type
    * Simplified anyio implementation

--- src/telnetio/_machine.py
+++ src/telnetio/_machine.py
@@ -1,14 +1,10 @@
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, Generator, Iterable, Iterator, List, Optional, Union
+from enum import Enum, auto
+from typing import Dict, List, Optional
 
-from ._opt import Opt
-
-StateCoroutine = Generator[None, int, None]
-BacklogItem = Union["Event", bytes]
-EventCallback = Callable[["Event"], Any]
-DataCallback = Callable[[bytes], Any]
+from telnetio import opt
 
 NUL = 0
 LF = 10
@@ -26,6 +22,33 @@ class TelnetOption:
     reply_pending: bool = False
 
 
+class State(Enum):
+    #: Receiving normal data
+    DATA = auto()
+    #: Receiving data after CR received
+    DATA_CR = auto()
+    #: IAC
+    COMMAND = auto()
+    #: IAC with multiple (3) bytes
+    NEGOTIATION = auto()
+    #: Subnegotation option
+    SUBN_OPTION = auto()
+    #: Subnegotation sub-opion
+    SUBN_SUBOPTION = auto()
+    #: Subnegotation data
+    SUBN_DATA = auto()
+    #: Subnegotation end
+    SUBN_END = auto()
+
+
+class ErrorKind(Enum):
+    SE_BUFFER_EMPTY = auto()
+    SE_BUFFER_NUL = auto()
+    SE_BUFFER_TOO_SHORT = auto()
+    SB_INVALID = auto()
+    OTHER = auto()
+
+
 class Event(ABC):
     @abstractmethod
     def as_bytes(self) -> bytes:  # pragma: nocover
@@ -33,12 +56,29 @@ class Event(ABC):
 
 
 @dataclass(frozen=True)
+class Error(Event):
+    kind: ErrorKind
+    data: bytes = b""
+
+    def as_bytes(self) -> bytes:  # pragma: nocover
+        raise NotImplementedError()
+
+
+@dataclass(frozen=True)
+class Data(Event):
+    msg: bytes
+
+    def as_bytes(self) -> bytes:
+        return self.msg
+
+
+@dataclass(frozen=True)
 class Command(Event):
-    cmd: Opt
+    cmd: int
     opt: Optional[int] = None  # only included for 3-byte commands
 
     def as_bytes(self) -> bytes:
-        data = bytes([Opt.IAC, self.cmd])
+        data = bytes([opt.IAC, self.cmd])
         if self.opt is not None:
             data += bytes([self.opt])
         return data
@@ -46,31 +86,11 @@ class Command(Event):
 
 @dataclass(frozen=True)
 class SubCommand(Event):
-    cmd: Opt
+    cmd: int
     opts: bytearray
 
     def as_bytes(self) -> bytes:
         return bytes([self.cmd]) + self.opts
-
-
-class Backlog(Iterable[BacklogItem]):
-    def __init__(self) -> None:
-        self.data: List[BacklogItem] = []
-
-    def __iter__(self) -> Iterator[BacklogItem]:
-        return iter(self.data)
-
-    def clear(self) -> None:
-        self.data.clear()
-
-    def add_message(self, data: bytes) -> None:
-        if not self.data or not isinstance(self.data[-1], bytes):
-            self.data.append(data)
-        else:
-            self.data[-1] += data
-
-    def add_event(self, event: Event) -> None:
-        self.data.append(event)
 
 
 class TelnetMachine:
@@ -79,192 +99,186 @@ class TelnetMachine:
     """
 
     #: List of IAC commands needing multiple (3) bytes
-    _iac_mbs = (Opt.DO, Opt.DONT, Opt.WILL, Opt.WONT)
+    _iac_mbs = (opt.DO, opt.DONT, opt.WILL, opt.WONT)
 
     def __init__(self) -> None:
-        self._state: StateCoroutine = self._state_data()
-        next(self._state)  # prime
-        self._next_state: Callable[[], StateCoroutine] = self._state_data
-        self._backlog = Backlog()
-        self._leftover = bytearray()
-        self._event_callbacks: List[EventCallback] = []
-        self._receive_callbacks: List[DataCallback] = []
-        self._send_callbacks: List[DataCallback] = []
+        self._state = State.DATA
+        self._buffer = bytearray()
 
-    def register_event_cb(self, callback: EventCallback) -> None:
-        """
-        Register a callback for every event created.
-        """
-        self._event_callbacks.append(callback)
+    def receive_data(self, data: bytes) -> List[Event]:
+        out: List[Event] = []
+        for char in data:
+            event = self._receive_byte(char)
+            if event is not None:
+                out.append(event)
+        return out
 
-    def register_receive_cb(self, callback: DataCallback) -> None:
-        """
-        Register a callback for message data that was received.
-        """
-        self._receive_callbacks.append(callback)
-
-    def register_send_cb(self, callback: DataCallback) -> None:
-        """
-        Register a callback for raw data that needs to be sent.
-        """
-        self._send_callbacks.append(callback)
-
-    def receive_data(self, data: bytes) -> None:
+    def _receive_byte(self, char: int) -> Optional[Event]:
         """
         Receive data from a stream into the state machine.
         """
-        data = self._leftover + data
-        self._leftover.clear()
-        for idx, byte in enumerate(data):
-            try:
-                self._state.send(byte)
-            except StopIteration:
-                self._state = self._next_state()
-                next(self._state)  # prime
-            except:
-                self._leftover.extend(data[idx:])
-                raise
+        # `int.to_bytes()` (57ns) is faster than `bytes([int])` (114ns)
+        char_byte = char.to_bytes(1, "little")  # endianness doesn't matter
 
-        for event_or_data in self._backlog:
-            if isinstance(event_or_data, bytes):
-                for recv_cb in self._receive_callbacks:
-                    recv_cb(event_or_data)
+        if self._state == State.DATA:
+            if char == opt.IAC:
+                self._state = State.COMMAND
+                return None
+            elif char == CR:
+                self._state = State.DATA_CR
+                return None
             else:
-                for event_cb in self._event_callbacks:
-                    event_cb(event_or_data)
-        self._backlog.clear()
+                return Data(char_byte)
 
-    def send(self, data: bytes) -> None:
-        """
-        Send raw data to the stream.
-        """
-        for cb in self._send_callbacks:
-            cb(data)
+        elif self._state == State.DATA_CR:
+            # previous char was CR
+            self._state = State.DATA
 
-    def send_message(self, data: bytes) -> None:
+            if char == LF:
+                return Data(b"\n")
+            elif char == NUL:
+                return Data(b"\r")
+            elif char == opt.IAC:
+                # IAC isn't allowed after \r according to the
+                # RFC, but handling this way is less surprising than
+                # delivering the IAC as normal data.
+                self._state = State.COMMAND
+                return Data(b"\r")
+            else:
+                return Data(b"\r" + char_byte)
+
+        elif self._state == State.COMMAND:
+            # received IAC
+            self._state = State.DATA
+
+            if char in self._iac_mbs:
+                self._buffer.append(char)
+                self._state = State.NEGOTIATION
+                return None
+            elif char == opt.SB:
+                self._state = State.SUBN_OPTION
+                return None
+            elif char == opt.IAC:
+                # escaped IAC
+                return Data(char_byte)
+            else:
+                return Command(char)
+
+        elif self._state == State.NEGOTIATION:
+            self._state = State.DATA
+            cmd = self._buffer.pop()
+            self._buffer.clear()
+            return Command(cmd, char)
+
+        elif self._state == State.SUBN_OPTION:
+            # opt.SB was sent
+            if char == opt.IAC:
+                self._state = State.SUBN_END
+                return None
+
+            self._buffer.append(char)
+            self._state = State.SUBN_SUBOPTION
+            return None
+
+        elif self._state == State.SUBN_SUBOPTION:
+            if char == opt.IAC:
+                self._state = State.SUBN_END
+                return None
+
+            self._buffer.append(char)
+            self._state = State.SUBN_DATA
+            return None
+
+        elif self._state == State.SUBN_DATA:
+            if char == opt.IAC:
+                self._state = State.SUBN_END
+                return None
+            else:
+                self._buffer.append(char)
+                return None
+
+        elif self._state == State.SUBN_END:
+            if char == opt.IAC:
+                # Repeated IAC is treated as data
+                self._state = State.SUBN_DATA
+                self._buffer.append(char)
+                return None
+            elif char == opt.SE:
+                self._state = State.DATA
+                buf = self._buffer.copy()
+                self._buffer.clear()
+
+                if not buf:
+                    return Error(ErrorKind.SE_BUFFER_EMPTY)
+                if buf[0] == opt.NULL:
+                    return Error(ErrorKind.SE_BUFFER_NUL)
+                if len(buf) == 1:
+                    return Error(ErrorKind.SE_BUFFER_TOO_SHORT, data=bytes(buf))
+
+                cmd = buf[0]
+                return SubCommand(cmd, buf[1:])
+            else:
+                self._buffer.clear()
+                self._state = State.DATA
+                return Error(ErrorKind.SB_INVALID, data=bytes([char]))
+        else:
+            raise RuntimeError(f"Unreachable state {self._state}")  # pragma: nocover
+
+    def send_message(self, data: bytes) -> bytes:
         """
         Send a message (text) to the stream.
         """
-        data = data.replace(bytes([Opt.IAC]), bytes([Opt.IAC, Opt.IAC]))
-        self.send(data)
+        return data.replace(bytes([opt.IAC]), bytes([opt.IAC, opt.IAC]))
 
-    def send_command(self, cmd: Opt, opt: Optional[int] = None) -> None:
-        self.send(Command(cmd, opt).as_bytes())
-
-    def _state_data(self) -> StateCoroutine:
-        while True:
-            char = yield
-
-            if char == Opt.IAC:
-                self._next_state = self._state_iac
-                return
-            elif char == CR:
-                char_after_cr = yield
-
-                if char_after_cr == LF:
-                    self._backlog.add_message(b"\n")
-                elif char_after_cr == NUL:
-                    self._backlog.add_message(b"\r")
-                elif char_after_cr == Opt.IAC:
-                    # IAC isn't allowed after \r according to the
-                    # RFC, but handling this way is less surprising than
-                    # delivering the IAC as normal data.
-                    self._backlog.add_message(b"\r")
-                    self._next_state = self._state_iac
-                    return
-                else:
-                    self._backlog.add_message(b"\r" + bytes([char_after_cr]))
-            else:
-                self._backlog.add_message(bytes([char]))
-
-    def _state_iac(self) -> StateCoroutine:
-        self._next_state = self._state_data
-
-        cmd = yield
-        if cmd in self._iac_mbs:
-            # These commands are 3-byte
-            opt = yield
-            self._backlog.add_event(Command(Opt(cmd), opt))
-        elif cmd == Opt.IAC:
-            # escaped IAC
-            self._backlog.add_message(bytes([Opt.IAC]))
-        elif cmd == Opt.SB:
-            self._next_state = self._state_sb
-        else:
-            self._backlog.add_event(Command(Opt(cmd)))
-
-    def _state_sb(self) -> StateCoroutine:
-        buf = bytearray()
-        self._next_state = self._state_data
-
-        while True:
-            data = yield
-            if data == Opt.IAC:
-                opt = yield
-                if opt == Opt.SE:
-                    if not buf:
-                        raise ValueError("SE: buffer empty")
-                    if buf[0] == Opt.NULL:
-                        raise ValueError("SE: buffer is NUL")
-                    if len(buf) == 1:
-                        raise ValueError("SE: buffer too short")
-
-                    cmd = Opt(buf[0])
-                    self._backlog.add_event(SubCommand(cmd, buf[1:]))
-                    break
-                elif opt == Opt.IAC:
-                    # escaped IAC
-                    buf.append(Opt.IAC)
-            else:
-                buf.append(data)
+    def send_command(self, cmd: int, opt: Optional[int] = None) -> bytes:
+        return Command(cmd, opt).as_bytes()
 
 
 class TelnetClient:
     def __init__(self, machine: TelnetMachine) -> None:
         self._machine = machine
-        self._machine.register_event_cb(self.on_event)
         self._options: Dict[int, TelnetOption] = defaultdict(TelnetOption)
 
     def on_event(self, event: Event) -> None:
         if isinstance(event, Command):
             if event.opt is None:
                 raise ValueError()
-            if event.cmd == Opt.DO:
+            if event.cmd == opt.DO:
                 return self.handle_do(event.opt)
-            elif event.cmd == Opt.DONT:
+            elif event.cmd == opt.DONT:
                 return self.handle_dont(event.opt)
-            elif event.cmd == Opt.WILL:
+            elif event.cmd == opt.WILL:
                 return self.handle_will(event.opt)
-            elif event.cmd == Opt.WONT:
+            elif event.cmd == opt.WONT:
                 return self.handle_wont(event.opt)
 
-    def check_local_option(self, option: Opt) -> bool:
+    def check_local_option(self, option: int) -> bool:
         return self._options[option].local_option or False
 
-    def check_remote_option(self, option: Opt) -> bool:
+    def check_remote_option(self, option: int) -> bool:
         return self._options[option].remote_option or False
 
     def handle_do(self, option: int) -> None:
         self._options[option].reply_pending = False
 
-        if option == Opt.ECHO:
+        if option == opt.ECHO:
             # DE requests us to echo their input
-            if not self._options[Opt.ECHO].local_option:
-                self._options[Opt.ECHO].local_option = True
-                self._machine.send_command(Opt.WILL, Opt.ECHO)
+            if not self._options[opt.ECHO].local_option:
+                self._options[opt.ECHO].local_option = True
+                self._machine.send_command(opt.WILL, opt.ECHO)
 
-        elif option == Opt.BINARY:
+        elif option == opt.BINARY:
             # DE requests us to receive BINARY
-            if not self._options[Opt.BINARY].local_option:
-                self._options[Opt.BINARY].local_option = True
-                self._machine.send_command(Opt.WILL, Opt.BINARY)
+            if not self._options[opt.BINARY].local_option:
+                self._options[opt.BINARY].local_option = True
+                self._machine.send_command(opt.WILL, opt.BINARY)
 
-        elif option == Opt.SGA:
+        elif option == opt.SGA:
             # DE wants us to suppress go-ahead
-            if not self._options[Opt.SGA].local_option:
-                self._options[Opt.SGA].local_option = True
-                self._machine.send_command(Opt.WILL, Opt.SGA)
-                self._machine.send_command(Opt.DO, Opt.SGA)
+            if not self._options[opt.SGA].local_option:
+                self._options[opt.SGA].local_option = True
+                self._machine.send_command(opt.WILL, opt.SGA)
+                self._machine.send_command(opt.DO, opt.SGA)
 
     def handle_dont(self, option: int) -> None:
         pass
